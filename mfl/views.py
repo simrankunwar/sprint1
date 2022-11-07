e4a17471a642e0556898978667786ae5250540a6
dsledge524 <80835514+dsledge524@users.noreply.github.com>
2022-11-07 13:12:57 -0600

    created book list page--no function yet

--- mfl/views.py
+++ mfl/views.py
@@ -9,72 +9,8 @@ def home(request):
     return render(request, 'mfl/home.html')
 
 
-def bookclub(request):
+def mylists(request):
     context = {
-        'posts': Post.objects.all()
+        'lists': Post.objects.all()
     }
-    return render(request, 'mfl/bookclub.html', context)
-
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'mfl/bookclub.html'
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
-    paginate_by = 5
-
-
-class UserPostListView(ListView):
-    model = Post
-    template_name = 'mfl/user_posts.html'
-    context_object_name = 'posts'
-    paginate_by = 5
-
-    def get_queryset(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted')
-
-
-class PostDetailView(DetailView):
-    model = Post
-
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    fields = ['title', 'content']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    fields = ['title', 'content']
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    success_url = '/'
-
-    def test_func(self):
-        post = self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
-
-
-def about(request):
-    return render(request, 'mfl/about.html', {'title': 'About'})
-
-
+    return render(request, 'mfl/mylists.html', context)
