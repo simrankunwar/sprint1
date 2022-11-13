3ee4219e5f0a42584a7e48098d52d2e62928fb52
simrankunwar <98556504+simrankunwar@users.noreply.github.com>
2022-11-13 15:52:47 -0600

    Update settings.py

--- library_bookstore/settings.py
+++ library_bookstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=hkbgmw02f!*-*n4(3w#8@_$*(*s_(ua_ipf6oqi&d@$v#hq)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
