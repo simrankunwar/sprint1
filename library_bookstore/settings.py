3272b22bf1923fe7e5c207c474d48e48dfc25999
simrankunwar <98556504+simrankunwar@users.noreply.github.com>
2022-11-13 13:24:12 -0600

    Update settings.py

--- library_bookstore/settings.py
+++ library_bookstore/settings.py
@@ -143,6 +143,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
